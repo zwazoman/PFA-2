20f59cdfe712ad0e838f9e2f9a045cdb0d2a1005
zwazoman <141416442+zwazoman@users.noreply.github.com>
2025-04-22 10:10:07 +0200

    audiomanager

---
+++ PFA-Episode2/Assets/_Scripts/Sound/Other/PlayerFootstepsHandler.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using mup;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public enum GroundType
+{
+    Concrete,
+    Grass
+}
+
+public class PlayerFootstepsHandler : MonoBehaviour
+{
+    [SerializeField] Transform _stepSocket;
+    [SerializeField] Terrain _terrain;
+
+    public void WalkStep(int isRunning = 0)
+    {
+        GroundType groundType = CheckGroundType();
+
+        Sounds sound = 0;
+
+        switch (groundType)
+        {
+            case GroundType.Concrete:
+                if (!Convert.ToBoolean(isRunning))
+                    sound = Sounds.WalkConcrete;
+                else
+                    sound = Sounds.RunConcrete;
+                    break;
+            case GroundType.Grass:
+                if (!Convert.ToBoolean(isRunning))
+                    sound = Sounds.WalkGrass;
+                else
+                    sound = Sounds.RunGrass;
+                break;
+        }
+
+        SFXManager.Instance.PlaySFXClipAtPosition(sound, _stepSocket.position, true);
+    }
+
+    GroundType CheckGroundType()
+    {
+        RaycastHit hit;
+        Physics.Raycast(_stepSocket.position, Vector3.down, out hit);
+
+        Terrain currentTerrain;
+        hit.collider.gameObject.TryGetComponent<Terrain>(out currentTerrain);
+
+        GroundType groundType = GroundType.Concrete;
+        return groundType;
+    }
+}
