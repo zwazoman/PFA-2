122a8413f406eda51d23116941524f5e530641b7
Nathan <63930671+nathan-exe@users.noreply.github.com>
2025-05-01 21:14:46 +0200

    hqe

--- PFA-Episode2/Assets/_Scripts/rendering/PostProcessManager.cs
+++ PFA-Episode2/Assets/_Scripts/rendering/PostProcessManager.cs
@@ -13,11 +13,6 @@ public class PostProcessManager :  MonoBehaviour
 
     void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        //RT = RenderTexture.GetTemporary(src.width , src.height , 0, RenderTextureFormat.DefaultHDR);
-
-        //Graphics.Blit(src, RT, postProcessMaterial, 0);
-        // postProcessMaterial.SetTexture("_CameraDepthTexture", src.depthBuffer);
-        // Read pixels from the source RenderTexture, apply the material, copy the updated results to the destination RenderTexture
         postProcessMaterial.SetVector("_resolution", new Vector2(src.width, src.height));
         Graphics.Blit(src, dest, postProcessMaterial);
     }
