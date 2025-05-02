a36ff57d282b8c91a97c48de225c58b255e3a4b6
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2025-05-02 14:54:33 +0200

    XTRYFHIJOP

--- PFA-Episode2/Assets/_Scripts/IngredientCase/ChooseIngredient.cs
+++ PFA-Episode2/Assets/_Scripts/IngredientCase/ChooseIngredient.cs
@@ -4,4 +4,21 @@ using System.Collections.Generic;
 public class ChooseIngredient : MonoBehaviour
 {
     [SerializeField] private List<Ingredient> _listScriptableIngredient = new();
+
+    private void Start()
+    {
+        ChooseRandomIngredient();
+    }
+
+    private void ChooseRandomIngredient()
+    {
+        List <Ingredient> listIngredient = _listScriptableIngredient;
+        int choice = Random.Range(0, listIngredient.Count - 1);
+        listIngredient.RemoveAt(choice);
+    }
+
+    private void SetupInfo()
+    {
+
+    }
 }
