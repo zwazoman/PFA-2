9652d6d49bba72355d3c351010519e8c2f27463c
MeguminFireFox <ambrehardy013@gmail.com>
2025-05-06 10:25:01 +0200

    zdgysy

--- PFA-Episode2/Assets/_Scripts/Game and items/Inventory.cs
+++ PFA-Episode2/Assets/_Scripts/Game and items/Inventory.cs
@@ -1,7 +1,23 @@
 using System.Collections.Generic;
+using UnityEngine;
 
-public class Inventory : ISavable<Inventory>
+public class Inventory : MonoBehaviour, ISavable<Inventory>
 {
+    #region Singleton
+    public static Inventory Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+    #endregion
+
     public List<SpellData> Spells = new();
     public List<Ingredient> Ingredients = new();
     public List<Sauce> Sauces = new();
