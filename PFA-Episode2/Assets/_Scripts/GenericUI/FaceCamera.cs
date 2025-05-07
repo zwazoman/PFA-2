1f2ccc4d3e4c8bb6c5a49fc6956c10780d81e203
nathan-exe <nathan.tazi.1@gmail.com>
2025-05-07 16:26:31 +0200

    UI

---
+++ PFA-Episode2/Assets/_Scripts/GenericUI/FaceCamera.cs
@@ -0,0 +1,36 @@
+using UnityEditor;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    [SerializeField] bool _OnlyOnStart;
+
+    public void LookAtCamera()
+    {
+        Vector3 targetDir =  transform.position - Camera.main.transform.position;
+        //Vector3 targetDir = Vector3.ProjectOnPlane(Camera.main.transform.position - transform.position, Camera.main.transform.forward);
+        transform.rotation = Quaternion.LookRotation(targetDir, Camera.main.transform.up);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        LookAtCamera();
+        enabled = !_OnlyOnStart;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtCamera();
+    }
+}
+
+[CustomEditor(typeof(FaceCamera))]
+class FaceCameraEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Look at camera")) ((FaceCamera)target).LookAtCamera();
+    }
+}
