5dcae4bc1a5c8bdbc67cd7e485422ee91c960f39
nathan-exe <nathan.tazi.1@gmail.com>
2025-05-12 15:46:21 +0200

    camera

--- PFA-Episode2/Assets/_Scripts/Combat/Camera/CameraBehaviour.cs
+++ PFA-Episode2/Assets/_Scripts/Combat/Camera/CameraBehaviour.cs
@@ -1,100 +1,29 @@
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 
 //controle la camera pendant les combats.
 public class CameraBehaviour : MonoBehaviour
 {
 
-    [Header("Movement")]
-    [SerializeField] float _smoothTime = 1;
+    Vector3 basePosition;
 
-    [Header("Mouse Input")]
-    [SerializeField] float _sensibility = 1;
-
-    [Header("Target Tracking")]
-    [SerializeField] float OptimalDistanceToTarget = 15;
-    [SerializeField] bool _autoUnfollowTargetOnMouseDrag = false;
-    [field: SerializeField] public Transform Target { get; private set; }
-    [field: SerializeField] public bool FollowTarget { get; private set; } = true;
-
-
-    Camera _cam;
-    Vector3 _vel;
-    Vector3 _worldMouvementInput;
-
-
-
-    /// <summary>
-    /// changes the Target Transform
-    /// </summary>
-    /// <param name="newTarget"></param>
-    public void SetTarget(Transform newTarget)
+    private void Start()
     {
-        Target = newTarget;
+        basePosition = transform.position;
+
+        CombatManager.Instance.OnNewTurn += OnNewTurn;
     }
 
-    /// <summary>
-    /// Starts following the current Target Transform
-    /// </summary>
-    /// <param name="newTarget"></param>
-    public void StartFollowingTarget(Transform newTarget = null)
+    void OnNewTurn(Entity e)
     {
-        if(newTarget!=null)Target = newTarget;
-        FollowTarget = true;
-    }
-
-    /// <summary>
-    /// Stops following the current Target Transform
-    /// </summary>
-    public void StopFollowingTarget()
-    {
-        FollowTarget = false;
-    }
-
-
-    private void Awake()
-    {
-        TryGetComponent(out _cam);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //mouse movement
-        HandleMouseMovement();
-
-        //target tracking
-        Vector3 targetPosition = transform.position;
-        FollowTarget &= Target != null;
-        if (FollowTarget)
+        if(e is PlayerEntity)
         {
-            targetPosition = Target.transform.position - transform.forward * OptimalDistanceToTarget;
-        }
-
-        //smoothDamp
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition + _worldMouvementInput, ref _vel, _smoothTime);
-    }
-
-
-
-
-    async UniTask HandleMouseMovement()
-    {
-        Vector3 oldPosition = Input.mousePosition;
-        await UniTask.Yield();
-
-        if (Input.GetMouseButton(1))
-        {
-            if (_autoUnfollowTargetOnMouseDrag && _worldMouvementInput != Vector3.zero) StopFollowingTarget();
-
-            Vector3 delta = Input.mousePosition - oldPosition;
-            _worldMouvementInput = delta.x * _cam.transform.right + delta.y * _cam.transform.up;
-            _worldMouvementInput.y = 0;
-            _worldMouvementInput *= -_sensibility;
+            transform.DOMove(basePosition, .8f).SetEase(Ease.InOutSine);
         }
         else
         {
-            _worldMouvementInput = Vector3.zero;
+            transform.DOMove(basePosition + Vector3.up*1f, .8f).SetEase(Ease.InOutSine);
         }
     }
 
