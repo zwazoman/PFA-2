a24dbaf4a39dea00df9a738867f65592e4a979fa
nathan-exe <nathan.tazi.1@gmail.com>
2025-05-13 12:56:03 +0200

    data validation

--- PFA-Episode2/Assets/_Scripts/Tests/Test_Data.cs
+++ PFA-Episode2/Assets/_Scripts/Tests/Test_Data.cs
@@ -8,7 +8,9 @@ public static class Test_Data
     [MenuItem("Data/Validate game data")]
     public static void ValidateGameData()
     {
+        
         Debug.Log("=== Validating Game Data ===");
+
 
         #region AOE
         Debug.Log("-- validating : Areas of effect --");
@@ -27,17 +29,17 @@ public static class Test_Data
             //tiles oustide of bounds
             foreach(Vector2Int t in AOE.AffectedTiles)
             {
-                if (!AOE.Bounds.Contains(t)) Debug.LogError("Y'a des tiles en dehors des bounds sur cet asset : " + path);
+                if (!AOE.Bounds.Contains(t)) Debug.LogError("Y'a des tiles en dehors des bounds sur cet asset : " + path, AOE);
             }
 
             //bounds max size
-            if (AOE.Bounds.width > 7 || AOE.Bounds.height > 7) Debug.LogError("les bounds sont trop gros ici : " + path);
+            if (AOE.Bounds.width > 7 || AOE.Bounds.height > 7) Debug.LogError("les bounds sont trop gros ici : " + path, AOE);
 
             //null sprite
-            if (AOE.sprite == null) Debug.LogError("Le sprite est null ici : " + path);
+            if (AOE.sprite == null) Debug.LogError("Le sprite est null ici : " + path, AOE);
 
             //null texture
-            if (AOE.sprite.texture == null) Debug.LogError("La texture du sprite est null ici : " + path);
+            if (AOE.sprite.texture == null) Debug.LogError("La texture du sprite est null ici : " + path,AOE);
 
         }
         #endregion
@@ -57,20 +59,21 @@ public static class Test_Data
             }
 
             //invalid name
-            if ((Ing.name == "")) Debug.LogError("cet ingredient n'a pas de nom : " + path);
+            if ((Ing.name == "")) Debug.LogError("cet ingredient n'a pas de nom : " + path, Ing);
 
             //null sprite
-            if ((Ing.sprite==null)) Debug.LogError("L'ingredient ici a pas de sprite : " + path);
+            if ((Ing.sprite==null)) Debug.LogError("L'ingredient ici a pas de sprite : " + path, Ing);
 
             //null sprite texture
-            if ((Ing.sprite == null)) Debug.LogError("le sprite de cet ingredient a pas de texture : " + path);
+            if ((Ing.sprite == null)) Debug.LogError("le sprite de cet ingredient a pas de texture : " + path, Ing);
 
             //0 value
-            if ((Ing.EffectValue == 0)) Debug.LogError("cet ingredient n'aura pas d'effet car sa valeur est 0 : " + path);
+            if ((Ing.EffectValue == 0)) Debug.LogError("cet ingredient n'aura pas d'effet car sa valeur est 0 : " + path,Ing);
             
 
         }
         #endregion
+
 
         #region Sauces
         Debug.Log("-- validating : Sauces --");
@@ -87,19 +90,20 @@ public static class Test_Data
             }
 
             //invalid name
-            if ((Sauce.name == "")) Debug.LogError("cette sauce n'a pas de nom : " + path);
+            if ((Sauce.name == "")) Debug.LogError("cette sauce n'a pas de nom : " + path, Sauce);
 
             //null sprite
-            if ((Sauce.sprite == null)) Debug.LogError("la sauce ici a pas de sprite : " + path);
+            if ((Sauce.sprite == null)) Debug.LogError("la sauce ici a pas de sprite : " + path, Sauce);
 
             //null sprite texture
-            if ((Sauce.sprite == null)) Debug.LogError("le sprite de cette sauce a pas de texture : " + path);
+            if ((Sauce.sprite == null)) Debug.LogError("le sprite de cette sauce a pas de texture : " + path, Sauce);
 
             //null area of effect
-            if ((Sauce.areaOfEffect == null)) Debug.LogError("Cette sauce n'a pas d'Area Of Effect : " + path);
+            if ((Sauce.areaOfEffect == null)) Debug.LogError("Cette sauce n'a pas d'Area Of Effect : " + path, Sauce);
 
         }
         #endregion
+
 
         #region Premade spells
         Debug.Log("-- validating : premade spells --");
@@ -116,20 +120,20 @@ public static class Test_Data
             }
 
             //invalid name
-            if ((asset.SpellData.Name == "")) Debug.LogError("ce spell n'a pas de nom : " + path);
+            if ((asset.SpellData.Name == "")) Debug.LogError("ce spell n'a pas de nom : " + path, asset);
 
             //null sprite
-            if ((asset.SpellData.Sprite == null)) Debug.LogError("ce spell n'a pas de sprite : " + path);
+            if ((asset.SpellData.Sprite == null)) Debug.LogError("ce spell n'a pas de sprite : " + path, asset);
 
             //null sprite texture
-            if ((asset.SpellData.Sprite.texture == null)) Debug.LogError("le sprite de ce spell n'a pas de texture : " + path);
+            if ((asset.SpellData.Sprite.texture == null)) Debug.LogError("le sprite de ce spell n'a pas de texture : " + path, asset);
 
             //effects
-            if (asset.SpellData.Effects.Count == 0) Debug.LogError("Ce spell n'a pas d'effet : " + path);
-            foreach(SpellEffect effect in asset.SpellData.Effects) if (effect.value == 0) Debug.LogError("Ce spell a un effet avec une valeur de 0 : " + path);
+            if (asset.SpellData.Effects.Count == 0) Debug.LogError("Ce spell n'a pas d'effet : " + path, asset);
+            foreach(SpellEffect effect in asset.SpellData.Effects) if (effect.value == 0) Debug.LogError("Ce spell a un effet avec une valeur de 0 : " + path, asset);
 
             //null area of effect
-            if ((asset.SpellData.AreaOfEffect == null)) Debug.LogError("ce spell n'a pas d'Area of effect : " + path);
+            if ((asset.SpellData.AreaOfEffect == null)) Debug.LogError("ce spell n'a pas d'Area of effect : " + path,asset);
 
         }
         #endregion
@@ -137,7 +141,7 @@ public static class Test_Data
 
         #region Entities
         Debug.Log("-- validating : entities --");
-        files = Directory.GetFiles("Assets/_Data/Entitites", "*.asset", SearchOption.TopDirectoryOnly);
+        files = Directory.GetFiles("Assets/_Data/Entities", "*.asset", SearchOption.TopDirectoryOnly);
         foreach (string path in files)
         {
             EnemyData Enemy = (EnemyData)AssetDatabase.LoadAssetAtPath(path, typeof(EnemyData));
@@ -145,7 +149,7 @@ public static class Test_Data
             //assets mal rangés
             if ((Enemy == null))
             {
-                Debug.LogError("Y'a un asset mal rangé ici connard : " + path, Enemy);
+                Debug.LogError("Y'a un asset mal rangé ici connard : " + path);
                 continue;
             }
 
@@ -163,6 +167,7 @@ public static class Test_Data
             foreach(PremadeSpell s in Enemy.Spells) if(s== null) Debug.LogError("Cet ennemi a une nullref dans ses spells. " + path, Enemy);
         }
         #endregion
+
 
         Debug.Log("=== Data Validation Complete ! ===");
 
