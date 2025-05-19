e48b552ee5d37c70992869f164a227023285cbf6
nathan-exe <nathan.tazi.1@gmail.com>
2025-05-19 11:47:03 +0200

    premade spell + ennemis

--- PFA-Episode2/Assets/_Scripts/Cooking/Scriptables/PremadeSpell.cs
+++ PFA-Episode2/Assets/_Scripts/Cooking/Scriptables/PremadeSpell.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "new spell", menuName = "Combat/PremadeSpell")]
 public class PremadeSpell : ScriptableObject
 {
+    public bool bien = false;
     public SpellData SpellData;
 }
