6d0f211ae34cc82d0e937e9e35f4c9d820f7d757
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2025-05-21 11:41:01 +0200

    QQSDF/
    sfhjk

--- PFA-Episode2/Assets/_Scripts/Map/MapBuildingTools.cs
+++ PFA-Episode2/Assets/_Scripts/Map/MapBuildingTools.cs
@@ -50,22 +50,22 @@ public class MapBuildingTools : MonoBehaviour
             CurrentPath.transform.localPosition = trailPos;
 
             // Rotation du sprite
-            Vector3 dir = PointB.transform.localPosition - PointA.transform.localPosition;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            //Vector3 dir = PointB.transform.localPosition - PointA.transform.localPosition;
+            //float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
             if ( PointA.transform.localPosition.y > PointB.transform.localPosition.y)
             {
-                CurrentPath.transform.localRotation = Quaternion.Euler(-90 + (-angle), 90, -90);
+                CurrentPath.transform.localRotation = Quaternion.Euler(25, 90, -90);
                 //CurrentPath.transform.localPosition = new Vector3(CurrentPath.transform.localPosition.x, CurrentPath.transform.localPosition.y - 50, CurrentPath.transform.localPosition.z);
             }
 
             else if (PointA.transform.localPosition.y < PointB.transform.localPosition.y) 
             { 
-                CurrentPath.transform.localRotation = Quaternion.Euler(90 + (-angle), 90, -90);
+                CurrentPath.transform.localRotation = Quaternion.Euler(-25, 90, -90);
                 //CurrentPath.transform.localPosition = new Vector3(CurrentPath.transform.localPosition.x, CurrentPath.transform.localPosition.y + 50, CurrentPath.transform.localPosition.z);
             }
 
-            else { CurrentPath.transform.localRotation = Quaternion.Euler(-90, 0, 0); }
+            else { CurrentPath.transform.localRotation = Quaternion.Euler(0, 90, -90); }
 
             PointA.PathBetweenNode.Add(CurrentPath);
             _savePath.Add(CurrentPath);
