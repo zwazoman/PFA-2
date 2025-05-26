b436fd5c56cd39859d8a1f37c03254ea640ad0ff
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2025-05-26 16:15:47 +0200

    RDTFYHI

--- PFA-Episode2/Assets/_Scripts/GenericUI/inventorySystem/InventoryMap.cs
+++ PFA-Episode2/Assets/_Scripts/GenericUI/inventorySystem/InventoryMap.cs
@@ -6,6 +6,6 @@ public class InventoryMap : MonoBehaviour
     [SerializeField] private Color _activeColor;
     [SerializeField] private Color _desactiveColor;
 
-    public void EnableColor(Image img) { img.color = _activeColor; img.transform.GetChild(1).gameObject.SetActive(true); }
+    public void EnableColor(Image img) { img.color = _activeColor; .SetActive(true); }
     public void DisableColor(Image img) { img.color = _desactiveColor; img.transform.GetChild(1).gameObject.SetActive(false); }
 }
