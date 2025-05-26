8cfb2f74556c8d6da5b3a1b0691f37914f82a731
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2025-05-26 22:51:21 +0200

    wsdfghn

---
+++ PFA-Episode2/Assets/_Scripts/GenericUI/inventorySystem/SetupUIInventory/GetInfoIngredient.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetInfoIngredient : MonoBehaviour
+{
+    public Image IngredientIcon;
+    public TextMeshProUGUI IngredientName;
+    public TextMeshProUGUI Effect;
+}
