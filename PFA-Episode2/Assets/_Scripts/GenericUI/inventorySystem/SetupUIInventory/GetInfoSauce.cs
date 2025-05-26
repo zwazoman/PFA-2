22af3111f2dc4769328f0695c388b9c7c354cbf9
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2025-05-26 23:08:10 +0200

    <qzwesdxh

---
+++ PFA-Episode2/Assets/_Scripts/GenericUI/inventorySystem/SetupUIInventory/GetInfoSauce.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetInfoSauce : MonoBehaviour
+{
+    public Image SauceIcon;
+    public TextMeshProUGUI SauceName;
+    public TextMeshProUGUI Effect;
+    public Image AoE;
+}
--- PFA-Episode2/Assets/_Scripts/GenericUI/inventorySystem/SetupWorldMapInventory.cs
+++ PFA-Episode2/Assets/_Scripts/GenericUI/inventorySystem/SetupWorldMapInventory.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class SetupWorldMapInventory : MonoBehaviour
 {
@@ -9,21 +7,37 @@ public class SetupWorldMapInventory : MonoBehaviour
     [SerializeField] private List<Transform> _sauceSlot = new();
 
     [Header("Prefab")]
-    [SerializeField] private GameObject _prefabItem;
+    [SerializeField] private GameObject _prefabIngredient;
+    [SerializeField] private GameObject _prefabSauce;
 
     private GetInfoIngredient _infoIngredientUI;
+    private GetInfoSauce _infoSauceUI;
     private Ingredient _ingredientChoose;
-    
+    private Sauce _sauceChoose;
+
     public void SetupIngredient()
     {
         for (int i = 0; i < GameManager.Instance.playerInventory.Ingredients.Count; i++)
         {
             _ingredientChoose = GameManager.Instance.playerInventory.Ingredients[i];
-            GameObject go = Instantiate(_prefabItem, _ingredientSlot[i]); //Création 
+            GameObject go = Instantiate(_prefabIngredient, _ingredientSlot[i]); //Création 
             _infoIngredientUI = go.GetComponent<GetInfoIngredient>();
             _infoIngredientUI.IngredientIcon.sprite = _ingredientChoose.sprite;
             //_infoIngredientUI.IngredientName.text = _ingredientChoose.name;
             //_infoIngredientUI.Effect.text = Serializer.GetIngredientEffectString(_ingredientChoose);
+        }
+    }
+
+    public void SetupSauce()
+    {
+        for (int i = 0; i < GameManager.Instance.playerInventory.Sauces.Count; i++)
+        {
+            _sauceChoose = GameManager.Instance.playerInventory.Sauces[i];
+            GameObject go = Instantiate(_prefabSauce, _sauceSlot[i]); //Création 
+            _infoSauceUI = go.GetComponent<GetInfoSauce>();
+            _infoSauceUI.SauceIcon.sprite = _sauceChoose.sprite;
+            //_infoSauceUI.IngredientName.text = _sauceChoose.name;
+            //_infoSauceUI.Effect.text = Serializer.GetIngredientEffectString(_sauceChoose);
         }
     }
 }
