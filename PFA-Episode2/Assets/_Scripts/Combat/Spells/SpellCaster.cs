35c02963382f6e341598044a3da6fe2175e23193
zwazoman <141416442+zwazoman@users.noreply.github.com>
2025-06-03 15:22:09 +0200

    zedg
    azef

--- PFA-Episode2/Assets/_Scripts/Combat/Spells/SpellCaster.cs
+++ PFA-Episode2/Assets/_Scripts/Combat/Spells/SpellCaster.cs
@@ -235,8 +235,6 @@ public class SpellCaster : MonoBehaviour
 
     void SummonEntityAtPoint(WayPoint point)
     {
-        print("summon");
-
         GameObject kamikaze = Instantiate(GameManager.Instance.staticData.kamikaze, new Vector3(point.transform.position.x, .5f, point.transform.position.z), Quaternion.identity);
         Entity entity = kamikaze.GetComponent<Entity>();
 
@@ -394,8 +392,6 @@ public class SpellCaster : MonoBehaviour
             return false;
         }
 
-        print("LANCE");
-
         PlayerEntity playerCastingEntity = null;
 
         if (castingEntity is PlayerEntity)
@@ -447,7 +443,6 @@ public class SpellCaster : MonoBehaviour
 
     async UniTask UtilitaryBehaviour(Spell spell, SpellCastData zoneData, WayPoint target)
     {
-        print("aaa");
         BakedUtilitarySpellEffect e = ComputeUtilitarySpellEffect(spell, ref zoneData);
         await ApplyUtilitarySpell(e, spell);
     }
@@ -486,10 +481,8 @@ public class SpellCaster : MonoBehaviour
 
     async UniTask ApplyUtilitarySpell(BakedUtilitarySpellEffect effect, Spell spell)
     {
-        print("ooo");
         if (effect.summonPoint != null)
         {
-            print("eeee");
             SpellProjectile projectile;
             Vector3 spawnPos;
             if (_spellCastingSocket != null)
