5cbc7e584c9ba2388dc04b1a46724bc1863f490b
Nathan <63930671+nathan-exe@users.noreply.github.com>
2025-06-15 15:45:00 +0200

    build Icon

--- PFA-Episode2/Assets/_Scripts/GenericUI/inventorySystem/DraggableSpellContainer.cs
+++ PFA-Episode2/Assets/_Scripts/GenericUI/inventorySystem/DraggableSpellContainer.cs
@@ -33,12 +33,14 @@ public class DraggableSpellContainer : DraggableItemContainer
         _descriptionPanel?.gameObject.SetActive(false);
         EventClicked += () =>
         {
+            Debug.Log("clicked");
             _descriptionPanel?.gameObject.SetActive(true);
             _descriptionPanel.ClearParent();
             
         };
         EventClickedSomewhereElse += () =>
         {
+            Debug.Log("clicked somewhere else");
             _descriptionPanel?.gameObject.SetActive(false);
             _descriptionPanel.AttachToTransform(transform) ;
         };
