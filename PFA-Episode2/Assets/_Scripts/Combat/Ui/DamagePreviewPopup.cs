3c2fa560258e0432afdd18bbd73bc156954f7633
Nathan <63930671+nathan-exe@users.noreply.github.com>
2025-06-17 15:31:45 +0200

    damage preview

--- PFA-Episode2/Assets/_Scripts/Combat/Ui/DamagePreviewPopup.cs
+++ PFA-Episode2/Assets/_Scripts/Combat/Ui/DamagePreviewPopup.cs
@@ -1,16 +1,28 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class DamagePreviewPopup : AnimatedPanel
+public class DamagePreviewPopup : MonoBehaviour
 {
 
     [SerializeField] private TMP_Text _text;
-    public void UpdateVisuals(int currentHealth, int currentShield, int shieldDiff, int healthDiff)
+    [SerializeField] CanvasGroup _canvasGroup;
+
+    public void Show()
+    {
+        _canvasGroup.DOFade(1,.15f);
+    }
+    public void Hide()
+    {
+        _canvasGroup.DOFade(0,.15f);
+    }
+    
+    public void UpdateVisuals(int currentHealth, int currentShield, int healthDiff, int shieldDiff )
     {
         if (healthDiff != 0)
         {
-            _text.text = $"{currentHealth}<color=#ff8080>({healthDiff})</color> <sprite name=heart>";
+            _text.text = $"{currentHealth}<color=#ff8080>{healthDiff}</color> <sprite name=heart>";
         }
         else
         {
@@ -23,17 +35,17 @@ public class DamagePreviewPopup : AnimatedPanel
             if (shieldDiff != 0)
             {
                 string color = shieldDiff<0? "#ff8080" : "#8080ff";
-                _text.text +=$" {currentShield}<color=#ff8080>({shieldDiff}) </color><sprite name=shield> ";
+                _text.text +=$"  {currentShield}<color=#ff8080>{shieldDiff} </color><sprite name=shield> ";
             }
             else
             {
-                _text.text +=$" {currentShield} <sprite name=shield> ";
+                _text.text +=$"  {currentShield} <sprite name=shield> ";
             }
         }
         else if (shieldDiff != 0)
         {
             string color = shieldDiff<0? "#ff8080" : "#8080ff";
-            _text.text +=$" {currentShield}<color=#ff8080>({shieldDiff}) </color><sprite name=shield> ";
+            _text.text += $" {currentShield}<color=#ff8080>"+(shieldDiff>0?"+":"")+$"{shieldDiff} </color><sprite name=shield> ";
         }
 
         
