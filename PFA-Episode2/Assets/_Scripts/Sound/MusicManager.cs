fe1fd35e952e83c2a91fcd63458850aa2872e2a4
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2025-06-17 13:44:53 +0200

    Heal de 20, correction UI ingredient

--- PFA-Episode2/Assets/_Scripts/Sound/MusicManager.cs
+++ PFA-Episode2/Assets/_Scripts/Sound/MusicManager.cs
@@ -31,7 +31,14 @@ public class MusicManager : MonoBehaviour
 
     private void Awake()
     {
-        instance = this;
+        if (instance == this || instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
     }
 
     private void Start()
